2972dcfc5b321edcfc404607c81835c8398223bd
Lukas Nehrke <lukasnehrke@protonmail.com>
2023-03-25 09:29:28 +0000

    feat: update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,14 +17,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body>
-        <header className="mx-auto flex max-w-4xl flex-wrap items-center justify-between px-4 pt-6 md:pt-8">
-          <h1 className="mr-8 overflow-hidden whitespace-nowrap font-semibold">
-            <Link href="/">&lt;lukasnehrke /&gt;</Link>
-          </h1>
-          <nav className="flex space-x-2 overflow-hidden py-3 text-sm text-indigo-600">
-            <Link href="/projects">Projects</Link>
-            <Link href="/contact">Contact</Link>
-          </nav>
+        <header className="mx-auto max-w-4xl border-b px-4 pt-2 md:border-none md:pt-8">
+          <div className="flex w-full flex-wrap items-center justify-between md:border-b">
+            <h1 className="mr-8 overflow-hidden whitespace-nowrap font-semibold">
+              <Link href="/">LN</Link>
+            </h1>
+            <nav className="flex space-x-2 overflow-hidden py-3 text-sm text-indigo-600">
+              <Link href="/projects">Projects</Link>
+              <Link href="/contact">Contact</Link>
+            </nav>
+          </div>
         </header>
         {children}
       </body>
