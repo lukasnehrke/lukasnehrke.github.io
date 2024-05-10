6f4cbb928976a35cc7ac56886839860b76574fae
Lukas Nehrke <lukasnehrke@protonmail.com>
2024-05-10 09:08:16 +0200

    feat: cleanup

--- app/header.tsx
+++ app/header.tsx
@@ -1,7 +1,9 @@
 import { ThemeToggle } from "@/components/theme-toggle";
 import { Github } from "lucide-react";
-import { Button } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 import { Nav } from "@/app/nav";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 export function Header(): JSX.Element {
   return (
@@ -10,11 +12,14 @@ export function Header(): JSX.Element {
         <Nav />
         <div className="flex items-center justify-center">
           <ThemeToggle />
-          <a href="https://github.com/lukasnehrke" target="_blank" rel="noopener noreferrer">
-            <Button variant="ghost" size="icon">
-              <Github className="h-4 w-4" />
-            </Button>
-          </a>
+          <Link
+            className={cn(buttonVariants({ variant: "ghost", size: "icon" }), "rounded-full")}
+            href="https://github.com/lukasnehrke"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Github className="h-4 w-4" />
+          </Link>
         </div>
       </div>
     </header>
